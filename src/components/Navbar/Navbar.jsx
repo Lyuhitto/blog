d9057973baa0f5efb8465b2667a4738a15758923
Lyuhitto <contact@sujungkim.dev>
2023-01-30 21:00:42 +0900

    Set blog logo to navbar
    Set blog logo to navbar.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
-import {container, menus} from './Navbar.module.css'
+import {container, logo, menus} from './Navbar.module.css'
 // import './Navbar.module.css'
+import blogLogo from './../../images/blogLogo.png'
 
 
 export default function Navbar() {
   return (
     <div className={container}>
+      <div className={logo}>
+        <img src={blogLogo} alt="홈으로 가기" />
+      </div>
       <ul className={menus}>
         {menusList.map((item, key) => (
           <li key={key}>
