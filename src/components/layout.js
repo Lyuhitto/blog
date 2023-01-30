a2da5d8002f1b7f9a9bdc9df2d5193a8dff9569c
Lyuhitto <contact@sujungkim.dev>
2023-01-30 18:18:22 +0900

    Add initial footer
    Add initial footer component.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,14 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import Navbar from './Navbar/Navbar'
+import Navbar from "./Navbar/Navbar"
+import Footer from "./Footer/Footer"
 
 const Layout = ({ children }) => {
-
   return (
     <div className="global-wrapper">
-      <Navbar  />
+      <Navbar />
       <main>{children}</main>
+      <Footer />
     </div>
   )
 }
