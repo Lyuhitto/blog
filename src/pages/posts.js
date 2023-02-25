d8066d7ff95417ff66c5840110d005f84c31e70b
Lyuhitto <contact@sujungkim.dev>
2023-02-26 02:08:17 +0900

    Implement search function
    Implement search function.

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -7,13 +7,18 @@ import Seo from "../components/seo"
 import { useState } from "react"
 
 export default function Posts({
-  data: {
-    allMarkdownRemark: { group },
-    site: {
-      siteMetadata: { title },
-    },
-  },
+  // data: {
+  //   allMarkdownRemark: { group },
+  //   site: {
+  //     siteMetadata: { title },
+  //   },
+  // },
+  data,
+  location,
 }) {
+  const group = data.allMarkdownRemark.group
+  const allPosts = data.allMarkdownRemark.edges
+
   const [tagList, setTagList] = useState([])
   const onClickTag = e => {
     const targetTag = e.target.value
@@ -26,9 +31,41 @@ export default function Posts({
 
     setTagList(v => v.sort())
   }
+
+  const [filtered, setFiltered] = useState({
+    filteredPosts: [],
+    query: "",
+  })
+  const handleSearchChange = event => {
+    const query = event.target.value
+    const filteredPosts = allPosts.filter(post => {
+      const { description, title, tags } = post.node.frontmatter
+      return (
+        // input에 입력된 검색어가 desc, title, tags에 해당되는지 검색
+        description.toLowerCase().includes(query.toLowerCase()) ||
+        title.toLowerCase().includes(query.toLowerCase()) ||
+        (tags && tags.join("").toLowerCase().includes(query.toLowerCase()))
+      )
+    })
+    setFiltered({
+      query,
+      filteredPosts,
+    })
+  }
+
+  const posts = filtered.query ? filtered.filteredPosts : allPosts
+
   return (
     <Layout>
       <p>현재 개의 포스트가 있습니다</p>
+      <h3>검색하기</h3>
+      <input
+        type="text"
+        aria-label="검색하기"
+        placeholder="검색어를 입력하세요"
+        value={filtered.query}
+        onChange={handleSearchChange}
+      ></input>
       <h3>태그들</h3>
       <section>
         {group.map(tag => (
@@ -42,8 +79,50 @@ export default function Posts({
         ))}
       </section>
       <p>현재선택 :{tagList.join(", ")}</p>
+
       <h3>포스트들</h3>
-      <ul></ul>
+      {posts.length === 0 ? (
+        <div>해당하는 포스트가 없습니다.</div>
+      ) : (
+        <ol>
+          {posts.map(({ node }) => {
+            const { excerpt } = node
+            const { slug } = node.fields
+            const { title, date, description, tags } = node.frontmatter
+
+            return (
+              <li key={slug}>
+                <article
+                  className="post-list-item"
+                  itemScope
+                  itemType="http://schema.org/Article"
+                >
+                  <header>
+                    <h2>
+                      <Link to={slug} itemProp="url">
+                        <span itemProp="headline">{title}</span>
+                      </Link>
+                    </h2>
+                    <small>{date}</small>
+                  </header>
+                  <section>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: description || excerpt,
+                      }}
+                      itemProp="description"
+                    />
+                  </section>
+                  <ul>
+                    {tags &&
+                      tags.map((tag, idx) => <li>{tag}</li>)}
+                  </ul>
+                </article>
+              </li>
+            )
+          })}
+        </ol>
+      )}
     </Layout>
   )
 }
@@ -56,10 +135,24 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(limit: 2000) {
+    allMarkdownRemark(limit: 2000, sort: { frontmatter: { date: DESC } }) {
       group(field: { frontmatter: { tags: SELECT } }) {
         fieldValue
         totalCount
+      }
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+            tags
+          }
+        }
       }
     }
   }
